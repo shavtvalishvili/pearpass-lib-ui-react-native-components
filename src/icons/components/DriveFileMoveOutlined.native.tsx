d4fa9d39746268eb0a3123d731d56df14936baab
Giorgi Shavtvalishvili <gshavtvalishvili@gmail.com>
2026-04-02 15:06:14 +0400

    Fix DriveFileMoveOutlined icon path to use rightward arrow from design.

--- src/icons/components/DriveFileMoveOutlined.native.tsx
+++ src/icons/components/DriveFileMoveOutlined.native.tsx
@@ -3,10 +3,10 @@ import * as React from "react";
 import Svg, { Path } from "react-native-svg";
 import type { SvgProps } from "react-native-svg";
 const SvgDriveFileMoveOutlined = (props: SvgProps) => (
-  <Svg width={24} height={24} fill="none" viewBox="0 0 24 24" {...props}>
+  <Svg width={24} height={24} fill="none" viewBox="0 0 16.6667 13.3333" {...props}>
     <Path
       fill="currentColor"
-      d="M20 6h-8l-2-2H4c-1.1 0-2 .9-2 2v12c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V8c0-1.1-.9-2-2-2zm0 12H4V6h5.17l2 2H20zm-6-3h2l-3-3-3 3h2v3h2z"
+      d="M15 1.66667H8.33333L6.66667 0H1.66667C0.75 0 0 0.75 0 1.66667V11.6667C0 12.5833 0.75 13.3333 1.66667 13.3333H15C15.9167 13.3333 16.6667 12.5833 16.6667 11.6667V3.33333C16.6667 2.41667 15.9167 1.66667 15 1.66667ZM15 11.6667H1.66667V1.66667H5.975L7.64167 3.33333H15V11.6667ZM8.46667 6.66667H5V8.33333H8.46667L7.14167 9.65833L8.325 10.8333L11.6667 7.50833L8.325 4.16667L7.15 5.34167L8.46667 6.66667Z"
     />
   </Svg>
 );
